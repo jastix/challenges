96c8f904006add9742a1ebeed639bc202a71a9f4
Nikolay Petrachkov <nik@jastix.biz>
2014-05-11 16:11:22 +0400

    Add Easy#Odd_numbers in GO

---
+++ CodeEval/Easy/12_Odd_Numbers/odd_numbers.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 99; i = i + 2 {
+        //'scanner.Text()' represents the test case, do something with it
+        fmt.Println(i)
+    }
+}
