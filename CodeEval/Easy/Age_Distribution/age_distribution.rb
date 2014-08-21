72e8d5d0d44249b4a5fe06a3537edd487ab4af0b
Nikolay Petrachkov <nik@jastix.biz>
2014-08-21 10:20:56 +0200

    Add Easy#Age_Distribution in Ruby

---
+++ CodeEval/Easy/Age_Distribution/age_distribution.rb
@@ -0,0 +1,26 @@
+def determine_group(age)
+  case age
+  when 0..2
+    "Still in Mama's arms"
+  when 3..4
+    "Preschool Maniac"
+  when 5..11
+    "Elementary school"
+  when 12..14
+    "Middle school"
+  when  15..18
+    "High school"
+  when 19..22
+    "College"
+  when 23..65
+    "Working for the man"
+  when 66..100
+    "The Golden Years"
+  else
+    "This program is for humans"
+  end
+end
+
+File.open(ARGV[0]).each_line do |line|
+  puts determine_group(line.chomp.to_i)
+end
