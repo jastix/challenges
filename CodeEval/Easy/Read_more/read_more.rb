f72d6233624864ced90111b9cbf4c1e982e358e9
Nikolay Petrachkov <nik@jastix.biz>
2014-11-03 18:04:49 +0100

    Add CodeEval#Easy Read_More in Ruby. Partial

---
+++ CodeEval/Easy/Read_more/read_more.rb
@@ -0,0 +1,19 @@
+# CodeEval#Easy Read More
+# You are given a text. Write a program which outputs its lines according to the following rules:
+#
+# If line length is ≤ 55 characters, print it without any changes.
+# If the line length is > 55 characters, change it as follows:
+#  - Trim the line to 40 characters.
+#  - If there are spaces ‘ ’ in the resulting string, trim it once again to the last space (the space should be trimmed too).
+#  - Add a string ‘... <Read More>’ to the end of the resulting string and print it.
+
+File.open(ARGV[0]).each_line do |line|
+  unless line.chomp.empty?
+    if line.length <= 55
+      puts line
+    else
+      idx = line[0..40].rstrip.rindex(" ")
+      puts idx == nil ? "#{line[0..40].rstrip}... <Read More>" : "#{line[0...idx]}... <Read More>" 
+    end
+  end
+end
